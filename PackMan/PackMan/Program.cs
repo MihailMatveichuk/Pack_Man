3479fe9c707bcdbb2806757120b8a68552f6d6bb
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2023-08-13 15:33:35 +0300

    feat: add movement of packman

--- PackMan/PackMan/Program.cs
+++ PackMan/PackMan/Program.cs
@@ -7,12 +7,63 @@ namespace PackMan
     {
         static void Main(string[] args)
         {
+            Console.CursorVisible = false;
+            bool isPlaying = true;
             int packmanX, packmanY;
+            int packmanDX = 0, packmanDY = 1;
             char[,] map = ReadMap("map1", out packmanX, out packmanY);
 
             DrawMap(map);
+            while (isPlaying)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    ChangeDirection(key, ref packmanDX, ref packmanDY);
+                }
+                if (map[packmanX + packmanDX, packmanY + packmanDY] != '#')
+                {
+                    Move(ref packmanY, ref packmanX, packmanDX, packmanDY);
+                }
+
+            }
+
+        }
+
+        static void Move(ref int packmanY, ref int packmanX, int packmanDX, int packmanDY)
+        {
+            Console.SetCursorPosition(packmanY, packmanX);
+            Console.Write(' ');
+
+            packmanX = packmanX + packmanDX;
+            packmanY = packmanY + packmanDY;
+
             Console.SetCursorPosition(packmanY, packmanX);
             Console.Write('@');
+            System.Threading.Thread.Sleep(200);
+        }
+
+        static void ChangeDirection(ConsoleKeyInfo key, ref int packmanDX, ref int packmanDY)
+        {
+            switch (key.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    packmanDX = -1;
+                    packmanDY = 0;
+                    break;
+                case ConsoleKey.DownArrow:
+                    packmanDX = 1;
+                    packmanDY = 0;
+                    break;
+                case ConsoleKey.LeftArrow:
+                    packmanDX = 0;
+                    packmanDY = -1;
+                    break;
+                case ConsoleKey.RightArrow:
+                    packmanDX = 0;
+                    packmanDY = 1;
+                    break;
+            }
         }
 
         static void DrawMap(char[,] mapName)
